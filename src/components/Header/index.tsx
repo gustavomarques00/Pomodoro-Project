2c07154885b3a478f7a0f9b63ec55c7c36a88158
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-07-13 17:48:50 -0300

    Criação da Home Page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+import Logo from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="" />
+
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
